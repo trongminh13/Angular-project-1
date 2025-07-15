ebb94ee1246a7b3b5f5d5de31c9eafc872be1111
admin <admin@DESKTOP-DIIK3ID>
2025-07-15 17:27:53 +0700

    15-07

---
+++ src/app.js
@@ -0,0 +1,65 @@
+let number = document.querySelector('number');
+let counter = 0;
+const swiper = new Swiper('.wrapper', {
+// loop: true,
+// spaceBetween: 30,
+
+// autoplay: {
+//   delay:1000,
+// scrollTo: true,
+// SVGAnimatedNumberList: 5,
+//   disableOnInteraction: false,
+//   pauseOnMouseEnter: true,
+// },
+// pagination:{
+//   el: '.swiper-pagination',
+//   clickable: true,
+//   dunamicBullets: true,
+// },
+ spaceBetween: 0,
+  speed:5000,
+	direction: 'horizontal',
+  autoplay: {delay: 0},
+
+  loop: true,
+  slidesPerView: 1,
+  freeMode: true,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+// breakpoints: {
+  // 640: {
+  //   slidesPerView: 1,
+  //   spaceBetween: 20,
+  // },
+  // 768: {
+  //   slidesPerView: 2,
+  //   spaceBetween: 40,
+  // },
+  // 1024: {
+  //   slidesPerView: 3,
+  //   spaceBetween: 50,
+  // },
+},
+
+// scrollbar: {
+//   el: '.swiper-scrollbar',
+// },
+});
+
+setInterval(() => {
+  if (counter == 65) {
+    clearInterval;
+  }else{
+    counter+= 1;
+    number.innerHTML = '${counter}%';
+  }
+},30);
